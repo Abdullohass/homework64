d7dec2d46c36321aa3ec62c8362e43fde8c2adf2
Abdullohass <abdulloh.s.gk@gmail.com>
2024-11-21 16:06:20 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const swiperContainer = document.querySelector('swiper-container');
+swiperContainer.swiper.on('slideChange', () => { 
+  const activeSlide = swiperContainer.querySelector('.swiper-slide-active');
+  const color = activeSlide.getAttribute('data-color');     
+  document.body.style.backgroundColor = color;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
